b86e2fef331bdcbf3d72911d0219347d5debacfb
Dmitry Zuikov <dzuikov@gmail.com>
2011-08-03 12:16:53 +0400

    Nested let flattening

---
+++ src/Compilers/Hopc/Frontend/LetFlatten.hs
@@ -0,0 +1,17 @@
+module Compilers.Hopc.Frontend.LetFlatten where
+
+import Data.Generics.Biplate
+import Control.Monad.State
+
+import Compilers.Hopc.Frontend.KTree
+
+{-
+
+(let (x (let (y e1) e2)) e3) => (let (y e1) (let (x e2) e3))
+
+-}
+
+flatten :: KTree -> KTree
+flatten k = rewriteBi tr k
+    where tr (KLet x (KLet y e1 e2) e3) = Just $ (KLet y e1 (KLet x e2 e3))
+          tr x = Nothing
--- src/Hopc.hs
+++ src/Hopc.hs
@@ -10,6 +10,7 @@ import Compilers.Hopc.Frontend.KTree
 import qualified Compilers.Hopc.Frontend.Lisp.KNormalize as K
 import qualified Compilers.Hopc.Frontend.AlphaConv as A
 import qualified Compilers.Hopc.Frontend.BetaReduction as B
+import qualified Compilers.Hopc.Frontend.LetFlatten as L
 --import Compilers.Hopc.Backend.DumbC
 import Debug.Trace
 
@@ -19,8 +20,7 @@ main = do
     (x:_) <- getArgs
     e <- withInput parseExpr x
     let k = either (const $ error "Parse error") K.kNormalizeExp e
-    let k' = B.betaReduce $ A.alphaConv k
---    let k' = A.alphaConv k
+    let k' = L.flatten $ B.betaReduce $ A.alphaConv k
     putStrLn $ prettyShow k'
     error "done"
 
