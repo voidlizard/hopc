3d59dcb8aa61e7850ef7fb6bf88ec0ffa5307fb4
Dmitry Zuikov <dzuikov@gmail.com>
2011-08-08 22:22:21 +0400

    cls : free vars in progress

--- src/Compilers/Hopc/Frontend/Closure.hs
+++ src/Compilers/Hopc/Frontend/Closure.hs
@@ -29,7 +29,7 @@ data Closure =  CInt Integer
              deriving (Show, Eq, Data, Typeable)
 
 
-data Conv = Conv { fns :: [(KId, Fun)], known :: S.Set KId } deriving (Show)
+data Conv = Conv { fns :: M.Map KId Fun, known :: S.Set KId, free :: S.Set KId } deriving (Show)
 
 type ConvM = State Conv
 
@@ -37,7 +37,7 @@ convert :: KTree -> Closure
 convert k = 
     let (cls, s) = runState (conv k) convInit
         binds = bindsOfCls cls
-    in CLetR (map bindsOfFn (fns s) ++ binds) (cOfCls cls)
+    in CLetR (map bindsOfFn (M.toList (fns s)) ++ binds) (cOfCls cls)
     where bindsOfCls (CLet n e1 e2) = [(n, e1)]
           bindsOfCls (CLetR binds e2) = binds
           bindsOfCls x = []
@@ -45,64 +45,69 @@ convert k =
           cOfCls (CLetR _ e) = e
           cOfCls e = e
           bindsOfFn (n, f@(Fun nm args free c)) = (fname n, CFun f)
---            let free' = filter (not.(flip elem args)) $ [n | CVar n <- universe c] ++ [n | CApplCls n _ <- universe c]
---            in (fname n, f) --CFun (Fun nm args free' c))
 
 conv KUnit = return CUnit 
 conv (KInt n) = return $ CInt n
 conv (KStr s) = return $ CStr s
-conv (KVar n) = return $ CVar n
+
+conv (KVar n) = addFree n >> (return . CVar) n
 
 conv (KLet n e1 e2) = do
     (n', e1') <- convBind (n, e1)
     e2' <- conv e2
     return $ CLet n e1' e2'
 
-conv (KLetR binds e2) = do 
+conv (KLetR binds e2) = do
     binds' <- forM binds convBind
     e2' <- conv e2
     return $ CLetR binds' e2'
 
-conv (KApp n args) = do
-    return $ CApplCls n args
+conv (KApp n args) = addFree n >> (return $ CApplCls n args)
 
 conv wtf = error $ "WTF? " ++ show wtf
 
 convBind (n, e@(KLambda args eb)) = do
     addKnown n
-    mapM_ addKnown args
-    e' <- conv eb
-    fv <- freeVars args eb
-    addFun n args fv e'
-    return $ (n, CMakeCls (fname n))
+    knowns' <- gets known
+    let knowns = S.difference knowns' (S.fromList args)
+    st@(Conv { fns = fs }) <- get
+    trace ("KNOWNS " ++ n ++ " " ++ show knowns ++ " ARGS " ++ show args) $ do
+        let (e', s') = runState (conv eb) (st {known = knowns})
+        st <- put $ st { fns = M.insert n (funOf n args ((S.toList.free) s') e') fs `M.union` (fns s') }
+        return $ (n, CMakeCls (fname n))
+--        trace ("FREE " ++ n + " " ++ (show (free s')) $ do
+--        addFun n args ((S.toList.free) s') e'
 
 convBind (n, e) = do
     addKnown n
     e' <- conv e
     return $ (n, e')
 
-convInit = Conv [] S.empty
+convInit = Conv M.empty S.empty S.empty
 
-isKnown :: KId -> ConvM Bool
-isKnown n = gets (\(Conv { known = kn }) -> S.member n kn )
+addFree :: KId -> ConvM ()
+addFree n = do
+    knowns <- gets known
+    if S.member n knowns
+        then addFree' n
+        else return ()
+    where  addFree' :: KId -> ConvM () 
+           addFree' n = do
+               st@(Conv { free = fr }) <- get
+               put $ st { free = S.insert n fr }
 
-addKnown :: KId -> ConvM ()
 addKnown n = do
-    s@(Conv { known = k }) <- get
-    put $ s { known = S.insert n k }
+    s@(Conv { known = kn }) <- get
+    put $ s { known = S.insert n kn }
 
 addFun :: KId -> [KId] -> [KId] -> Closure -> ConvM ()
 addFun n args free bdy = do
     s@(Conv { fns = fs }) <- get
-    put $ s { fns = fs ++ [ (n, Fun (fname n) args free bdy )] }
+    put $ s { fns = M.insert n (funOf n args free bdy) fs }
+
+funOf n args free bdy = (Fun (fname n) args free bdy)
 
 fname n = "fun_" ++ n
-
-freeVars args cls = do
-    let v1 = [n | KVar n   <- universe cls]
-    let v2 = [n | KApp n _ <- universe cls]
-    knowns <- filterM isKnown (v1 ++ v2)
-    return $ filter (not.(flip elem args) ) knowns
 
 instance Pretty Closure where
     pPrintPrec _ _ (CUnit)  = text "()" 
